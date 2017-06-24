ef12bd24895aaf0e18d9a4aa747b8bbda7d7a719
vanhoofseppe <vanhoofseppe@gmail.com>
2017-06-25 00:30:39 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#Liwal WebApp
+#http://localhost:8080/
+"""
+main.py
+"""
+from bottle import route, run, template, TEMPLATE_PATH
+
+TEMPLATE_PATH.insert(0, '/storage/emulated/0/qpython/projects3/Liwal/views')
+
+import controller.home
+
+run(host='10.0.0.57', port=8080)
